a1def9ca4325ae29dafd729ca17d531ce87a3083
Roee Tzabari <tzabari.roee@gmail.com>
2024-11-22 16:24:02 +0200

    Completed all the tasks.

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,47 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		
+		String s1 = preProcess(str1), s2 = preProcess(str2);
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+		char c;
+		for (int i = 0; i < s1.length(); i++) {
+			c = s1.charAt(i);
+			if (s2.indexOf(c) == -1) {
+				return false;
+			}
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		char l;
+		String newStr = "";
+		for (int i = 0; i < str.length(); i++) {
+			l = str.charAt(i);
+			if ((l > 'a' && l < 'z') || (l > 'A' && l < 'Z')) {
+				newStr += ("" + l).toLowerCase();
+			}
+		}
+		return newStr;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		
+		String newStr = "";
+		int random;
+		while (str.length() > 0) {
+			random = (int)(Math.random() * str.length());
+			newStr += str.charAt(random);
+			str = str.replace("" + str.charAt(random), "");
+		}
+		return newStr;
 	}
 }
--- LoanCalc.java
+++ LoanCalc.java
@@ -7,11 +7,13 @@ public class LoanCalc {
 	// Gets the loan data and computes the periodical payment.
     // Expects to get three command-line arguments: loan amount (double),
     // interest rate (double, as a percentage), and number of payments (int).  
+
 	public static void main(String[] args) {		
 		// Gets the loan data
 		double loan = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
+
 		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 
 		// Computes the periodical payment using brute force search
@@ -28,8 +30,14 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		
+		double x = loan;
+
+		for (int i = 0; i < n; i++) {
+			x = (x - payment) * rate;
+		}
+
+		return x;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +46,15 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		double g = loan / n;
+		iterationCounter = 0;
+
+		while (endBalance(loan, rate, n, g) > 0) {
+			g += epsilon;
+			iterationCounter++;
+		}
+		
+		return g;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -48,7 +63,23 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+        
+		iterationCounter = 0;
+		double L = 1;
+		double H = loan;
+		double g = (L + H) / 2;
+
+		while (H - L > epsilon) {
+			if (endBalance(loan, rate, n, g) * endBalance(loan, rate, n, L) > 0) {
+				L = g;
+			} 
+			else {
+				H = g;
+			}
+			g = (L + H) / 2;
+			iterationCounter++;
+		}
+
+		return g;
     }
 }
